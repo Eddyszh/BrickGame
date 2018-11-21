17520bb56c1af06aa0169cce4bfeff2dd9365072
Eddy Zapata <eddyszh@gmail.com>
2018-11-21 17:24:01 -0500

    Adds level 3

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSize;
+
+    Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+        transform.position = startPosition + Vector3.back * newPosition;
+    }
+}
---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    int randomDirection;
+    public int speed;
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Move());
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {		
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, 1f, 8f), 13, 0);
+	}
+
+    IEnumerator Move()
+    {
+        randomDirection = Random.Range(0, 2);
+        if (randomDirection == 0) transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+        if (randomDirection == 1) transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        yield return new WaitForSeconds(2);
+    }
+}
